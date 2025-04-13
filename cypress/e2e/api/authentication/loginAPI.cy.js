1832b934e798980d87e497a941c017cfc6003864
Clark Ewerton <clarkewerton@hotmail.com>
2025-04-12 21:07:24 -0300

    Update loginAPI.cy.js

--- cypress/e2e/api/authentication/loginAPI.cy.js
+++ cypress/e2e/api/authentication/loginAPI.cy.js
@@ -1,23 +1,16 @@
-import { faker } from '@faker-js/faker/locale/en';
+import { UserFactory } from '../../../factories/userFactory'; 
 
 describe('Login Functionalities API', () => {
   let login_401, login_400;
-  
-  const randomName = faker.name.firstName().toLowerCase();
-  const testUser = {
-    email: `${randomName}testClark@example.com`,
-    name: `${randomName} ${faker.name.lastName().toLowerCase()} testClark`,
-    username: randomName + 'testClark',
-    password: faker.internet.password(),
-    administrador: "false",
-  };
 
   beforeEach(() => {
-    // Carrega fixtures antes dos testes
+    // load fixtures before testing
     cy.fixture('login_401').then((data) => (login_401 = data));
     cy.fixture('login_400').then((data) => (login_400 = data));
 
-    // Deleta usuários apenas uma vez antes da suíte de testes
+    newUser = UserFactory.validUser();
+
+    // delete user that has name TestClark
     cy.deleteAllUsersButRootAndTestClark();
   });
 
@@ -26,20 +19,20 @@ describe('Login Functionalities API', () => {
      * A `api_loginAndSignUp` já faz asserções para garantir que o login ocorreu,
      * por isso esse teste não precisa de asserts extras.
      */
-    cy.api_loginAndSignUp(testUser.email, testUser.password, testUser.administrador);
+    cy.api_loginAndSignUp(newUser.email, newUser.password, newUser.administrador);
   });
 
   it('Validate login throws an error when using invalid credentials', () => {
-    cy.api_login(testUser.email, testUser.password).then((response) => {
+    cy.api_login(newUser.email, newUser.password).then((response) => {
       expect(response.status).to.eq(401);
       expect(response.body).to.deep.equal(login_401);
     });
   });
 
   it('Validate login throws an error when using invalid email format', () => {
-    const invalidEmail = "test";
+    const invalidUser = UserFactory.invalidEmailUser();  // Using the user with invalid email
     
-    cy.api_login(invalidEmail, testUser.password).then((response) => {
+    cy.api_login(invalidUser.email, newUser.password).then((response) => {
       expect(response.status).to.eq(400);
       expect(response.body).to.deep.equal(login_400);
     });
