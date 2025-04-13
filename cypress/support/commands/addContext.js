96f5144400cd234f74d9784e495b46b73e071f5a
Clark Ewerton <clarkewerton@hotmail.com>
2025-04-13 17:56:29 -0300

    Update addContext.js

--- cypress/support/commands/addContext.js
+++ cypress/support/commands/addContext.js
@@ -2,8 +2,8 @@ import addContext from 'mochawesome/addContext'
 
 Cypress.on("test:after:run", (test, runnable) => {
     let videoName = Cypress.spec.name
-    videoName = videoName.replace(/\.js.*$/, '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
+    // Adiciona o nome completo da spec para garantir nomes únicos
+    const videoUrl = 'videos/' + videoName.replace('.js', '') + '.mp4'
 
     addContext({ test }, videoUrl)
 })
